c698a0548c500020629eab7d562a575f634c04ff
tamanugi <tamanugi@gmail.com>
2017-05-24 23:04:34 +0900

    implemented responders

--- lib/responders/lgtm.ex
+++ lib/responders/lgtm.ex
@@ -1,2 +1,41 @@
 defmodule HedwigLgtm.Responders.Lgtm do
+  use Hedwig.Responder
+
+  @lgtm_url "https://lgtm.in/g/"
+
+  @usage """
+  <text> (lgtm) - Replies with a random Borat image.
+  """
+  respond ~r/lgtm$/i, msg do
+
+    lgtm_image_url = fetch() |> find_image_url
+
+    reply msg, lgtm_image_url
+
+  end
+
+  defp fetch do
+    headers = ["Accept": "application/json"]
+    opts = [ hackney: [:insecure], follow_redirect: true ]
+
+    HTTPoison.get(@lgtm_url, headers, opts)
+    |> handle_response
+  end
+
+  defp handle_response({:ok, %{status_code: 200, body: body}}) do
+    {:ok, Poison.Parser.parse!(body)}
+  end
+
+  defp handle_response({_, %{status_code: _status, body: _body}}) do
+    {:error}
+  end
+
+  defp find_image_url({:ok, json}) do
+    json
+    |> Map.get("actualImageUrl")
+  end
+
+  defp find_image_url({:error}) do
+    "Error Occured."
+  end
 end
