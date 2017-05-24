1cadc1a3b880fa4df58f0860fbb3888681e2fc27
tamanugi <tamanugi@gmail.com>
2017-05-24 23:09:10 +0900

    fix test-description

--- test/responders/lgtm_test.exs
+++ test/responders/lgtm_test.exs
@@ -4,7 +4,7 @@ defmodule HedwigLgtm.Responders.LgtmTest  do
   @moduletag start_robot: true, name: "fukurou", responders: [{HedwigLgtm.Responders.Lgtm, []}]
 
   describe "" do
-    test "responds with a LGTM image url", %{adapter: adapter, msg: msg} do
+    test "lgtm responds with a LGTM image url", %{adapter: adapter, msg: msg} do
       send adapter, {:message, %{msg | text: "fukurou lgtm"}}
       assert_receive {:message, %{text: text}}, 5000
       assert text =~ "testuser: http"
